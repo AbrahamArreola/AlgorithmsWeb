2fc61fe4dfcfa7d1b8ada58c7b3ffc5776e359a8
Abraham Arreola <abrahamarrolab198@gmail.com>
2020-06-21 18:12:46 -0500

    Second page added

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('backtracking.html')
 
+@app.route('/dijkstra')
+def dijkstra():
+    return render_template('dijkstra.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
